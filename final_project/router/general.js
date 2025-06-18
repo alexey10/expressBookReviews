e1a9bccc96433bce4c5cf7c51b8a904f8491de5d
Alexey Piskovatskov <p_alexey@yahoo.com>
2025-06-18 19:46:07 -0400

    Update title endpoint

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -59,10 +59,19 @@ public_users.get('/author/:author',function (req, res) {
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.get('/title/:title', function (req, res) {
+    const title = req.params.title;
+  
+    // Filter the books to find all matching the given title
+    const matchingBooks = Object.values(books).filter(book => book.title.toLowerCase() === title.toLowerCase());
+    
+    if (matchingBooks.length > 0) {
+      res.status(200).send(JSON.stringify(matchingBooks, null, 4));
+    } else {
+      res.status(404).json({ message: "No books found for the given title" });
+    }
+  });
+  
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
